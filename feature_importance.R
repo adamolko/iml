2442d00c9f6e2139d938b8e8dff96275d84971c9
Daniel <daniel.racek@chello.at>
2020-11-24 21:44:47 +0100

    Cleaned code for SHAP feature importance Added permutation feature importance

--- feature_importance.R
+++ feature_importance.R
@@ -25,67 +25,40 @@ training_data = readRDS(paste0(path, "/results/training_data.rds"))
 #Shap Feature Importance
 shap_values <- shap.values(xgb_model = xgmodel$learner.model, X_train = select(training_data, - SalePrice))
 
+shap_values$shap_score = shap_values$shap_score %>% select(sort(current_vars()))
+
+
 #Group together effects of categorical variables
-shap_values$shap_score$MSZoning =  shap_values$shap_score %>% select(MSZoning.RL: MSZoning.RH) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(MSZoning.RL: MSZoning.RH))
 
-shap_values$shap_score$Neighborhood =  shap_values$shap_score %>% select(Neighborhood.CollgCr: Neighborhood.Blueste) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(Neighborhood.CollgCr: Neighborhood.Blueste))
+list_categories = c(c("MSZoning", "MSZoning.C..all.", "MSZoning.RM"),
+                    c("LotShape","LotShape.IR1","LotShape.Reg"),
+                    c("BldgType","BldgType.1Fam","BldgType.TwnhsE"),
+                    c("BsmtFinType1","BsmtFinType1.ALQ","BsmtFinType1.Unf"),
+                    c("BsmtQual","BsmtQual.Ex","BsmtQual.TA"),
+                    c("Electrical","Electrical.Fuse.or.Mix","Electrical.SBrkr"),
+                    c("ExterCond","ExterCond.1","ExterCond.3"),
+                    c("ExterQual","ExterQual.Ex","ExterQual.TA"),
+                    c("FireplaceQu","FireplaceQu.AA","FireplaceQu.TA"),
+                    c("Foundation","Foundation.BrkTil","Foundation.PConc"),
+                    c("Functional","Functional.Maj","Functional.Typ"),
+                    c("HeatingQC","HeatingQC.Ex","HeatingQC.TA"),
+                    c("KitchenQual","KitchenQual.Ex","KitchenQual.TA"),
+                    c("Neighborhood","Neighborhood.Blmngtn","Neighborhood.Veenker"),
+                    c("porch_type","porch_type.enclosed_porch","porch_type.wood_deck"),
+                    c("RoofStyle","RoofStyle.Gable","RoofStyle.Other"),
+                    c("SaleCondition","SaleCondition.Abnorml","SaleCondition.Partial"),
+                    c("SaleType","SaleType.COD","SaleType.WD"),
+                    c("SeasonSold","SeasonSold.a","SeasonSold.w")
+                    )
 
-shap_values$shap_score$LotShape =  shap_values$shap_score %>% select(LotShape.Reg: LotShape.IR2.5) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(LotShape.Reg: LotShape.IR2.5))
-
-shap_values$shap_score$BldgType =  shap_values$shap_score %>% select(BldgType.1Fam: BldgType.Twnhs) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(BldgType.1Fam: BldgType.Twnhs))
-
-shap_values$shap_score$RoofStyle =  shap_values$shap_score %>% select(RoofStyle.Gable: RoofStyle.Other) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(RoofStyle.Gable: RoofStyle.Other))
-
-shap_values$shap_score$ExterQual =  shap_values$shap_score %>% select(ExterQual.Gd: ExterQual.Fa) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(ExterQual.Gd: ExterQual.Fa))
-
-shap_values$shap_score$ExterQual =  shap_values$shap_score %>% select(ExterQual.Gd: ExterQual.Fa) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(ExterQual.Gd: ExterQual.Fa))
-
-shap_values$shap_score$ExterCond =  shap_values$shap_score %>% select(ExterCond.2: ExterCond.1) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(ExterCond.2: ExterCond.1))
-
-shap_values$shap_score$Foundation =  shap_values$shap_score %>% select(Foundation.PConc: Foundation.Other) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(Foundation.PConc: Foundation.Other))
-
-shap_values$shap_score$BsmtQual =  shap_values$shap_score %>% select(BsmtQual.Gd: BsmtQual.No) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(BsmtQual.Gd: BsmtQual.No))
-
-shap_values$shap_score$BsmtFinType1 =  shap_values$shap_score %>% select(BsmtFinType1.GLQ: BsmtFinType1.No) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(BsmtFinType1.GLQ: BsmtFinType1.No))
-
-shap_values$shap_score$HeatingQ =  shap_values$shap_score %>% select(HeatingQC.Ex: HeatingQC.Po) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(HeatingQC.Ex: HeatingQC.Po))
-
-shap_values$shap_score$Electrical =  shap_values$shap_score %>% select(Electrical.SBrkr: Electrical.Fuse.or.Mix) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(Electrical.SBrkr: Electrical.Fuse.or.Mix))
-
-shap_values$shap_score$KitchenQual =  shap_values$shap_score %>% select(KitchenQual.Gd: KitchenQual.Fa) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(KitchenQual.Gd: KitchenQual.Fa))
-
-shap_values$shap_score$Functional =  shap_values$shap_score %>% select(Functional.Typ: Functional.Maj) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(Functional.Typ: Functional.Maj))
-
-shap_values$shap_score$FireplaceQu =  shap_values$shap_score %>% select(FireplaceQu.TA: FireplaceQu.No) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(FireplaceQu.TA: FireplaceQu.No))
-
-shap_values$shap_score$SaleType =  shap_values$shap_score %>% select(SaleType.WD: SaleType.Other) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(SaleType.WD: SaleType.Other))
-
-shap_values$shap_score$SaleCondition =  shap_values$shap_score %>% select(SaleCondition.Normal: SaleCondition.Family) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(SaleCondition.Normal: SaleCondition.Family))
-
-shap_values$shap_score$SeasonSold =  shap_values$shap_score %>% select(SeasonSold.w: SeasonSold.a) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(SeasonSold.w: SeasonSold.a))
-
-shap_values$shap_score$porch_type =  shap_values$shap_score %>% select(porch_type.enclosed_porch: porch_type.wood_deck) %>% rowSums()
-shap_values$shap_score = select(shap_values$shap_score,  -(porch_type.enclosed_porch: porch_type.wood_deck))
-
+for(i in seq(from=1, to=length(list_categories), by=3)){
+  name = list_categories[i]
+  from =  list_categories[i+1]
+  to =  list_categories[i+2]
+  shap_values$shap_score[[name]] = shap_values$shap_score %>% select(from: to) %>% rowSums()
+  shap_values$shap_score = select(shap_values$shap_score,  -(from: to))
+  
+}
 shap_values$mean_shap_score =  shap_values$shap_score %>% summarise_all(~ mean(abs(.)))
 shap_values$shap_score = shap_values$shap_score %>% select(sort(current_vars()))
 shap_values$mean_shap_score = shap_values$mean_shap_score %>% select(sort(current_vars()))
@@ -107,6 +80,38 @@ ggsave(filename = paste0(path, "/results/SHAP_feature_importance.jpg"), plot = p
 shap_values <- shap.values(xgb_model = xgmodel$learner.model, X_train = select(training_data, - SalePrice))
 shap_long <- shap.prep(shap_contrib = shap_values$shap_score, X_train = select(training_data, - SalePrice))
 p2 = shap.plot.summary(shap_long)
+p2
 ggsave(filename = paste0(path, "/results/SHAP_summary.jpg"), plot = p2)
+
+shap.plot.summary.wrap1(model = xgmodel$learner.model, X =  select(training_data, - SalePrice), top_n = 6, dilute = FALSE)
+
+xgb.plot.shap(data = as.matrix(select(training_data, - SalePrice)), model = xgmodel$learner.model, top_n = 6)
 #-----------------------------
+#Permutatuon Feature Importance
+
+mod <- Predictor$new(xgmodel, data = select(training_data, - SalePrice), y = select(training_data, SalePrice))
+imp <- FeatureImp$new(predictor = mod, loss = "rmsle", compare="difference")
+imp$results = imp$results %>% arrange(feature)
+for(i in seq(from=1, to=length(list_categories), by=3)){
+  name = list_categories[i]
+  from =  list_categories[i+1]
+  to =  list_categories[i+2]
+  
+  index_from = which(imp$results$feature == from)
+  index_to = which(imp$results$feature == to)
+  
+  new_row = colSums( xxx[index_from:index_to,2:5] )
+  imp$results = imp$results[-c(index_from:index_to), ]
+  
+  imp$results = imp$results %>% add_row(feature = name, importance.05 = new_row[1], importance = new_row[2], importance.95 = new_row[3], permutation.error = new_row[4])
+}
+plotting_data2 = imp$results
+p3<-ggplot(data=plotting_data2, aes(x=importance, y=reorder(feature, importance))) +
+  geom_bar(stat="identity", fill="steelblue") +
+  xlab("Feature Importance (Loss: RMSLE)") +
+  ylab("Feature")
+p3
+ggsave(filename = paste0(path, "/results/permutation_feature_importance.jpg"), plot = p3)
+
+
 
